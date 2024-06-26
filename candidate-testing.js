a82232f4112dc02a3a3392dba809b36bf1076047
K M <menon_kavita@yahoo.com>
2024-06-25 21:32:08 -0500

    Uploading TASK 1: MINIMUM VIABLE QUIZ - updated candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,13 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
+
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,18 +20,29 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("Please enter your name. ")
+  return candidateName;
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question).toLowerCase()                        // COnverting to Lowercase
+  return candidateAnswer;
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  correctAnswer = correctAnswer.toLowerCase()
+  candidateAnswer = candidateAnswer.toLowerCase()
+         
+  if(candidateAnswer === correctAnswer){
+          console.log("That's correct!")
+  }
+  else{
+          console.log("Sorry, that's incorrect! Please try again.")
+  }
 
 
 
@@ -40,9 +53,11 @@ function gradeQuiz(candidateAnswers) {
 }
 
 function runProgram() {
-  askForName();
+  let userName = askForName();
+  
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+  console.log("Hola!", userName);
+  
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
