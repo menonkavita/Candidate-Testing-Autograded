66212864357ac94cb21712feefa8d3e7061c8163
K M <menon_kavita@yahoo.com>
2024-06-26 22:07:58 -0500

    Assignment 1: Candidate Testing - Task 2: Multiple Questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,9 +13,14 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ",
+                "True or false: 5 kilometer == 5000 meters? ",
+                "(5 + 3)/2 * 10 = ? ",
+                "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+                "What is the minimum crew size for the ISS? "];
+
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -25,16 +30,24 @@ function askForName() {
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question).toLowerCase()                        // COnverting to Lowercase
-  return candidateAnswer;
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer   
+  /*candidateAnswer = input.question(question).toLowerCase()                                // Task 1 Code - Converting to Lowercase
+  return candidateAnswer;  */ 
+
+  for(let i = 0; i < questions.length; i++){                                                // Task 2 Code - Multiple Questions
+    candidateAnswers.push(input.question(questions[i]))
+  }
+
+//console.log("\n[askQuestion] Candidate Answers after accepting answers: ", candidateAnswers,"\n")     // Shows user answers while debuggin
+return candidateAnswers;
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  correctAnswer = correctAnswer.toLowerCase()
+  
+  /* correctAnswer = correctAnswer.toLowerCase()                                 // Task 1 Code - Single Question
   candidateAnswer = candidateAnswer.toLowerCase()
          
   if(candidateAnswer === correctAnswer){
@@ -42,12 +55,34 @@ function gradeQuiz(candidateAnswers) {
   }
   else{
           console.log("Sorry, that's incorrect! Please try again.")
-  }
+  } */
 
 
+   // Task 2 Code - Multiple Questions
+
+    
+    console.log(`\n                     ----------- RESULTS of the QUIZ -----------\n 
+                Question 1: ${questions[0]} 
+                Your Answer:  ${candidateAnswers[0]}. 
+                Correct Answer: ${correctAnswers[0]}\n
+                Question 2: ${questions[1]}  
+                Your Answer:  ${candidateAnswers[1]}. 
+                Correct Answer: ${correctAnswers[1]}\n
+                Question 3: ${questions[2]}  
+                Your Answer:  ${candidateAnswers[2]}. 
+                Correct Answer: ${correctAnswers[2]}\n
+                Question 4: ${questions[3]}  
+                Your Answer:  ${candidateAnswers[3]}. 
+                Correct Answer: ${correctAnswers[3]}\n
+                Question 5: ${questions[4]}  
+                Your Answer:  ${candidateAnswers[4]}. 
+                Correct Answer: ${correctAnswers[4]}
+                    ----------- ----------- -----------`);
+
+      
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+       //candidateAnswers[i] = candidateAnswers[i].toLowerCase()                               // Task 3 Code - Converting to Lowercase
 
   return grade;
 }
